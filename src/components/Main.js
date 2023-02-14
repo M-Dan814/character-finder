bcb33149d81992181b482432b9e779b47f57aa01
M-Dan814 <m.dayan5689@gmail.com>
2023-02-15 00:13:23 +0500

    Create Main component where game will be played

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+const Image = require("./Images/images.jpg");
+
+const Main = () => {
+
+  return (
+    <div>
+      <img alt="game" id="image" src={Image} />
+    </div>
+  );
+};
+
+export { Main };
