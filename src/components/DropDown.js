49de28106fc3b485504342e06d1a54ce67c59e34
M-Dan814 <m.dayan5689@gmail.com>
2023-02-15 23:07:06 +0500

    Create dropdown and functionality for it's display

---
+++ src/components/DropDown.js
@@ -0,0 +1,13 @@
+const DropDown = (props) => {
+  return (
+    <div className="dropdown">
+      <ul>
+        <li>Waldo</li>
+        <li>Waldo's Twin</li>
+        <li>Bernie Sanders</li>
+      </ul>
+    </div>
+  );
+};
+
+export { DropDown };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,30 @@
+import { DropDown } from "./DropDown";
+
 const Image = require("./Images/images.jpg");
 
 const Main = () => {
+  const listDisplay = (event) => {
+    const dropdown = document.querySelector(".dropdown");
+    const targetter = document.querySelector(".target")
+    if (dropdown.style.display !== "none" && targetter) {
+      dropdown.style.display = "none";
+      document.body.removeChild(targetter)
+    } else {
+      dropdown.style.display = "block";
+      dropdown.style.left = event.clientX + "px";
+      dropdown.style.top = event.clientY + "px";
+      const target = document.createElement("div");
+      target.classList.add("target");
+      target.style.top = `${event.clientY - 45}px`;
+      target.style.left = `${event.clientX - 45}px`;
+      document.body.append(target);
+    }
+  };
 
   return (
     <div>
-      <img alt="game" id="image" src={Image} />
+      <img alt="game" onClick={listDisplay} id="image" src={Image} />
+      <DropDown />
     </div>
   );
 };
